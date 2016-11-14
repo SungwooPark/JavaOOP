192fd50686873c811abf2f059ad1e53d91521196
Sung Park <sungwoo.park@students.olin.edu>
2016-11-14 18:01:58 -0500

    Add animaltest file

---
+++ code/src/code/AnimalTest.java
@@ -0,0 +1,28 @@
+package code;
+
+public class AnimalTest {
+
+	public static void main(String[] args) {
+		AnimalPack animals = new AnimalPack();
+		animals.makeArray();
+		
+		for (int i=0;i<5;i++){
+			if (Math.random() < 0.5){
+				Dog d = new Dog();
+				d.setSize(i);
+				animals.addAnimal(i, d);
+			}else{
+				Cat c = new Cat();
+				c.setSize(i);
+				animals.addAnimal(i, c);
+			}
+		}
+		
+		for (int i=0;i<animals.getSize();i++){
+			animals.getAnimal(i).makeNoise();
+		}
+
+		System.out.println(animals.biggestAnimal().getName());
+	}
+
+}
